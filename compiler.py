67f9a4d1675a35bdc2611ef6bbaecc157ab2a2ad
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-02-17 14:01:13 -0800

    Fix default argument for source_code

--- compiler.py
+++ compiler.py
@@ -8,7 +8,7 @@ defaultSourceFile = './RAT24S_programs/RAT24S.source'
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('source_code', metavar='Path to source code file', type=str,
-                    default=defaultSourceFile)
+                    nargs='?', default=defaultSourceFile)
 
     path = parser.parse_args().source_code
     with open(path, 'r') as sourceFile:
