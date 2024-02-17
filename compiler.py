01872de42aa11e8668a8a05c60d109eac9ea1216
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-02-17 12:47:11 -0800

    open source code file using with

--- compiler.py
+++ compiler.py
@@ -1,20 +1,21 @@
-import lexer
 import argparse
 import os
 
-defaultSourceFile = './RAT24S.source'
+import lexer
+
+defaultSourceFile = './RAT24S_programs/RAT24S.source'
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('source_code', metavar='Expected file', type=str,
+    parser.add_argument('source_code', metavar='Path to source code file', type=str,
                     nargs=1, default=defaultSourceFile)
 
-    sourceFile = open(str(parser.parse_args().source_code[0]), 'r')
-    sourceCode = sourceFile.read()
-    print(sourceCode)
-    l = lexer.Lexer(sourceCode)
-    print(l.tokenize())
-
+    path = parser.parse_args().source_code
+    with open(path, 'r') as sourceFile:
+        sourceCode = sourceFile.read()
+        print(sourceCode)
+        l = lexer.Lexer(sourceCode)
+        print(l.tokenize())
 
 if __name__ == "__main__":
     main()
