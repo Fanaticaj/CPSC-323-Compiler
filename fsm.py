3f8d615d05088a5a87193ce77e046db626141403
josephvelasquez48 <61490256+josephvelasquez48@users.noreply.github.com>
2024-02-29 16:18:42 -0800

    Update fsm.py

--- fsm.py
+++ fsm.py
@@ -89,12 +89,12 @@ class FSM:
 
     def is_integer(self, w):
         integer_tt = [
-            [1,1],
-            [1,1]
+            [1],
+            [1]
             ]
         
-        integer_alpha ={'0': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1,
-                      '7': 1, '8': 1, '9': 1}
+        integer_alpha ={'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0,
+                      '7': 0, '8': 0, '9': 0}
         integer_accepted = set([1])
         is_accepted = self.run_fsm(integer_tt, integer_alpha, integer_accepted, w)
         return is_accepted
