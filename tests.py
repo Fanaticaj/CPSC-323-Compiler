1e19a677e32bc8d5f5f3c5b84f24b513f7b97c86
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-03-13 20:02:36 -0700

    Finish Lexer tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from lexer import Lexer
+from parse_token import Token
 
 class TestLexer(unittest.TestCase):
     """Test that lexer works"""
@@ -21,31 +22,76 @@ class TestLexer(unittest.TestCase):
 
     def test_comments(self):
         """Test that comments are removed and not tokenized"""
-        raise NotImplementedError
+        source_code = "[* This is a one line comment *]"
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+
+        token_count = len(tokens)
+        self.assertEqual(token_count, 0)
 
     def test_keywords(self):
         """Test that keywords are tokenized as keywords"""
-        raise NotImplementedError
+        source_code = "boolean         else            endif"
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+        expected_tokens = [Token(type='keyword', value='boolean'), Token(type='keyword', value='else'), Token(type='keyword', value='endif')]
+
+        self.assertEqual(tokens, expected_tokens)
 
     def test_separators(self):
         """Test that separators are tokenized as separators"""
-        raise NotImplementedError
+        source_code = "(){},;$"
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+        expected_tokens = [
+                Token(type='separator', value='('), Token(type='separator', value=')'), Token(type='separator', value='{'), Token(type='separator', value='}'),
+                Token(type='separator', value=','), Token(type='separator', value=';'), Token(type='separator', value='$')
+            ]
+
+        self.assertEqual(tokens, expected_tokens)
 
     def test_operators(self):
         """Test that operators are tokenized as operators"""
-        raise NotImplementedError
+        source_code = "+ - * / == != > < <= >= ="
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+        expected_tokens = [
+                Token(type='operator', value='+'), Token(type='operator', value='-'), Token(type='operator', value='*'), Token(type='operator', value='/'),
+                Token(type='operator', value='=='), Token(type='operator', value='!='), Token(type='operator', value='>'), Token(type='operator', value='<'),
+                Token(type='operator', value='<='), Token(type='operator', value='>='), Token(type='operator', value='=')
+            ]
+
+        self.assertEqual(tokens, expected_tokens)
 
     def test_identifiers(self):
         """Test that identifiers are tokenized as identifiers"""
-        raise NotImplementedError
+        source_code = "abc123 testIdentifier variable_1"
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+        expected_tokens = [Token(type='identifier', value='abc123'), Token(type='identifier', value='testidentifier'), Token(type='identifier', value='variable_1')]
+
+        self.assertEqual(tokens, expected_tokens)
 
     def test_integers(self):
         """Test that integers are tokenized as integers"""
-        raise NotImplementedError
+        source_code = "7 153 1849375932"
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+        expected_tokens = [Token(type='integer', value='7'), Token(type='integer', value='153'), Token(type='integer', value='1849375932')]
+
+        self.assertEqual(tokens, expected_tokens)
 
     def test_reals(self):
         """Test that reals are tokenized as reals"""
-        raise NotImplementedError
+        source_code = "3.14 22.5 0.99 25.0 26. .009"
+        l = Lexer(source_code)
+        tokens = l.tokenize()
+        expected_tokens = [
+                Token(type='real', value='3.14'), Token(type='real', value='22.5'), Token(type='real', value='0.99'), Token(type='real', value='25.0'),
+                Token(type='illegal', value='26.'), Token(type='illegal', value='.009')
+            ]
+
+        self.assertEqual(tokens, expected_tokens)
 
 if __name__ == "__main__":
     unittest.main()
